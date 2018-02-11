a447ee098a803926274d1214b1088e244af9b41a
神樹桜乃 <kodama@sakuno.moe>
2018-02-11 22:29:23 +0800

    Add tests

---
+++ tests/Sakuno.SQLite.Tests/MemoryDatabaseFixture.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Sakuno.SQLite.Tests
+{
+    public class MemoryDatabaseFixture : IDisposable
+    {
+        public SQLiteDatabase Database { get; }
+
+        public MemoryDatabaseFixture()
+        {
+            Database = SQLiteDatabase.OpenMemoryDatabase();
+        }
+
+        public void Dispose() => Database.Dispose();
+    }
+}
