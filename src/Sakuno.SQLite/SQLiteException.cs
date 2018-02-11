aa24b6392ba824f999b12352ec960021789f389c
神樹桜乃 <kodama@sakuno.moe>
2018-02-11 21:52:46 +0800

    Add project

---
+++ src/Sakuno.SQLite/SQLiteException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Sakuno.SQLite
+{
+    public class SQLiteException : Exception
+    {
+        public SQLiteResultCode ErrorCode { get; }
+
+        public SQLiteException(SQLiteResultCode errorCode)
+        {
+            ErrorCode = errorCode;
+        }
+    }
+}
---
+++ src/Sakuno.SQLite/SQLiteObject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace Sakuno.SQLite
+{
+    public abstract class SQLiteObject : IDisposable
+    {
+        volatile int _isDisposed;
+        public bool IsClosed => _isDisposed != 0;
+
+        ~SQLiteObject() => Dispose(false);
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected void Dispose(bool disposing)
+        {
+            if (_isDisposed != 0 || Interlocked.CompareExchange(ref _isDisposed, 1, 0) != 0 || !disposing)
+                return;
+
+            DisposeManagedResource();
+        }
+        protected virtual void DisposeManagedResource() { }
+    }
+}
